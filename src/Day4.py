4b63415827f227bad7e925dfd86c6219f5460825
Abhinavk941 <abhinavkarnatak941@gmail.com>
2022-03-31 00:06:54 +0530

    Final Commit

--- src/Day4.py
+++ src/Day4.py
@@ -1,19 +1,10 @@
-# Python program to find the factorial of a number provided by the user.
-
-# change the value for a different result
-num = int(input('Enter a number : '))
-
-# To take input from the user
-#num = int(input("Enter a number: "))
-
+n = int(input('Enter a number : '))
 factorial = 1
-
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
+if n < 0:
+   print("Wrong Input")
+elif n == 0:
    print("The factorial of 0 is 1")
 else:
-   for i in range(1,num + 1):
+   for i in range(1,n + 1):
        factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+   print("The factorial of",n,"is",factorial)
