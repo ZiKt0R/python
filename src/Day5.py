a30c7c8f095c3ed945b93e27791f0fb6b379be8a
Abhinavk941 <abhinavkarnatak941@gmail.com>
2022-03-31 21:27:28 +0530

    Initial Commit

---
+++ src/Day5.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the number : "))                      
+p = len(str(n))                                           
+s = 0                                                     
+k = n                                                     
+while k>0:                                                
+    d = k%10                                              
+    s = s + (d**p)                                        
+    k = k//10                                             
+if n == s:                                                
+    print('{} is an Armstrong number'.format(n))          
+else:                                                       
+    print('{} is not an Armstrong number'.format(n))
