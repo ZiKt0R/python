f810a87514e9a5edec1cc5de8bf30f2673a4d5bf
Abhinavk941 <abhinavkarnatak941@gmail.com>
2022-04-02 12:02:05 +0530

    Latest Commit

--- Day3.py
+++ Day3.py
@@ -2,7 +2,7 @@ year_of_birth = int(input('Enter your birth year : '))
 month_of_birth = int(input('Enter your birth month : '))
 present_year = int(input('Enter present year : '))
 present_month = int(input('Enter present month : '))
-if (12 - month_of_birth + present_month)>12:
+if (12 - month_of_birth + present_month)>=12:
     print((present_year - year_of_birth - 0), 'years,', str(12 - month_of_birth + present_month-12), 'months')
 else:
     print(((present_year - year_of_birth) - 1), 'years,', str(12 - month_of_birth + present_month), 'months')
---
+++ src/Day7.py
@@ -0,0 +1,3 @@
+n = input('Enter an alphabet that should be printed : ')
+for i in range(0,10000):
+    print(n, end = '')
