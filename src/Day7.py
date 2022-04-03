2fa95a99c7f6a6c4ac94b8b4e448279fad836efe
Abhinavk941 <abhinavkarnatak941@gmail.com>
2022-04-03 21:12:31 +0530

    Final Commit

--- src/Day7.py
+++ src/Day7.py
@@ -1,3 +1,2 @@
-n = input('Enter an alphabet that should be printed : ')
-for i in range(0,10000):
-    print(n, end = '')
+X = 'abcdefghijklmnopqrstuvwxyzAK'
+print(X[26]+X[1]+X[7]+X[8]+X[13]+X[0]+X[21], X[27]+X[0]+X[17]+X[13]+X[0]+X[19]+X[0]+X[10])
